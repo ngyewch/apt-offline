b9731b4c17fba85eb7a8c4a70e175d4e8bd69c62
Nick Ng <ngyewch@gmail.com>
2024-07-31 11:16:52 +0800

    refactor: switched to urfave/cli

--- go.mod
+++ go.mod
@@ -5,36 +5,20 @@ go 1.22
 require (
 	github.com/Masterminds/semver/v3 v3.2.1
 	github.com/fsouza/go-dockerclient v1.11.1
-	github.com/ngyewch/go-versioninfo v1.5.1
-	github.com/spf13/cobra v1.8.1
+	github.com/urfave/cli/v2 v2.27.3
 )
 
 require (
-	dario.cat/mergo v1.0.0 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
-	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
-	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/containerd/containerd v1.6.26 // indirect
 	github.com/containerd/log v0.1.0 // indirect
-	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/docker/docker v27.0.3+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
-	github.com/emirpasic/gods v1.18.1 // indirect
-	github.com/fatih/color v1.10.0 // indirect
-	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
-	github.com/go-git/go-billy/v5 v5.5.0 // indirect
-	github.com/go-git/go-git/v5 v5.11.0 // indirect
-	github.com/goccy/go-yaml v1.11.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
-	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
@@ -43,16 +27,9 @@ require (
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc2.0.20221005185240-3a7f492d3f1b // indirect
-	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/skeema/knownhosts v1.2.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/xanzy/ssh-agent v0.3.3 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,155 @@
 package main
 
 import (
-	"github.com/ngyewch/apt-offline/cmd"
+	"fmt"
+	"github.com/ngyewch/apt-offline/downloader"
+	"github.com/ngyewch/apt-offline/dpkg"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	version         string
+	commit          string
+	commitTimestamp string
+
+	flagDownloadDir = &cli.PathFlag{
+		Name:     "download-dir",
+		Usage:    "download directory",
+		Required: true,
+	}
+	flagVersionCodename = &cli.StringFlag{
+		Name:     "version-codename",
+		Usage:    "Debian version codename",
+		Required: true,
+	}
+	flagArch = &cli.StringFlag{
+		Name:     "arch",
+		Usage:    "architecture",
+		Required: true,
+	}
+	flagDpkgStatus = &cli.PathFlag{
+		Name:  "dpkg-status",
+		Usage: "Path to /var/lib/dpkg/status file",
+	}
+	flagArchived = &cli.BoolFlag{
+		Name:  "archived",
+		Usage: "archived mode",
+	}
+
+	app = &cli.App{
+		Name:    "apt-offline",
+		Usage:   "apt-offline",
+		Version: version,
+		Commands: []*cli.Command{
+			{
+				Name:   "download",
+				Usage:  "download",
+				Action: doDownload,
+				Flags: []cli.Flag{
+					flagDownloadDir,
+					flagVersionCodename,
+					flagArch,
+					flagDpkgStatus,
+					flagArchived,
+				},
+			},
+		},
+	}
 )
 
 func main() {
-	cmd.Execute()
+	cli.VersionPrinter = func(cCtx *cli.Context) {
+		var parts []string
+		if version != "" {
+			parts = append(parts, fmt.Sprintf("version=%s", version))
+		}
+		if commit != "" {
+			parts = append(parts, fmt.Sprintf("commit=%s", commit))
+		}
+		if commitTimestamp != "" {
+			formattedCommitTimestamp := func(commitTimestamp string) string {
+				epochSeconds, err := strconv.ParseInt(commitTimestamp, 10, 64)
+				if err != nil {
+					return ""
+				}
+				t := time.Unix(epochSeconds, 0)
+				return t.Format(time.RFC3339)
+			}(commitTimestamp)
+			if formattedCommitTimestamp != "" {
+				parts = append(parts, fmt.Sprintf("commitTimestamp=%s", formattedCommitTimestamp))
+			}
+		}
+		fmt.Println(strings.Join(parts, " "))
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func doDownload(cCtx *cli.Context) error {
+	downloadDir := flagDownloadDir.Get(cCtx)
+	versionCodename := flagVersionCodename.Get(cCtx)
+	arch := flagArch.Get(cCtx)
+	dpkgStatus := flagDpkgStatus.Get(cCtx)
+	archived := flagArchived.Get(cCtx)
+
+	d := downloader.NewDownloader(versionCodename, archived)
+
+	err := d.Init()
+	if err != nil {
+		return err
+	}
+
+	err = d.Download(downloadDir, arch, cCtx.Args().Slice())
+	if err != nil {
+		return err
+	}
+
+	if dpkgStatus != "" {
+		f, err := os.Open(dpkgStatus)
+		if err != nil {
+			return err
+		}
+		defer func(f *os.File) {
+			_ = f.Close()
+		}(f)
+
+		packageStatuses, err := dpkg.ParsePackageStatuses(f)
+		if err != nil {
+			return err
+		}
+
+		dirEntries, err := os.ReadDir(downloadDir)
+		if err != nil {
+			return err
+		}
+
+		for _, dirEntry := range dirEntries {
+			if dirEntry.IsDir() {
+				continue
+			}
+			if !strings.HasSuffix(dirEntry.Name(), ".deb") {
+				continue
+			}
+			parts := strings.Split(dirEntry.Name()[0:len(dirEntry.Name())-4], "_")
+			packageStatus := packageStatuses.FindPackageStatus(parts[0])
+			if (packageStatus != nil) && (packageStatus.Status == "install ok installed") && (packageStatus.Architecture == arch) {
+				err = os.Remove(filepath.Join(downloadDir, dirEntry.Name()))
+				if err != nil {
+					return err
+				}
+				fmt.Printf("# removed %s\n", dirEntry.Name())
+			}
+		}
+	}
+
+	return nil
 }
